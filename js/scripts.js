e6050e94aeda3d194de78fb4a2f2a4a81acce92c
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-03-04 15:42:48 +0300

    add variable names - names and days of week

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,3 @@
-.
+let daysofTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
