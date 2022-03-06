3964b2461e5c2693a5d45ec3d6b1021989872839
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-03-06 13:32:21 +0300

    Add variable inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 let daysofTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+let yearValue = document.getElementById('year').value;
+let monthValue = document.getElementById('month').value;
+let dayValue = parseInt(document.getElementById('day').value);
