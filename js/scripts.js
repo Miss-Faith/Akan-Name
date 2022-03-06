105d32d931de863c506f6ea26b642c9ebe020753
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-03-06 13:37:35 +0300

    Include a max for the number of days captured

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,17 @@ let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 let yearValue = document.getElementById('year').value;
 let monthValue = document.getElementById('month').value;
 let dayValue = parseInt(document.getElementById('day').value);
+
+function dayLimit() {
+  let maxday = document.getElementById("day");
+  if (monthValue === "February" && parseInt(yearValue) % 4 === 0) {
+    maxday.max = 29;
+  }else if (monthValue === "February" && parseInt(yearValue) % 4 !== 0) {
+    maxday.max = 28;
+  }else if (monthValue === "January" || monthValue === "March" || monthValue === "May" || monthValue === "July" || monthValue === "August" || monthValue === "October" || monthValue === "December") {
+    maxday.max = 31;
+  }else {
+    maxday.max = 30;
+  }
+}
+dayLimit();
