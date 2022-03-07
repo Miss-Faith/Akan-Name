6d91539ea2a290665e8b57569d13cc18618d6fa3
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-03-07 10:34:59 +0300

    Update calculator workings

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-function getAkanName () {
+function getAkanName (event) {
+  event.preventDefault();
   let daysofTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   let maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   let femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
@@ -6,9 +7,9 @@ function getAkanName () {
   let yearValue = document.getElementById('year').value;
   let monthValue = document.getElementById('month').value;
   let dayValue = parseInt(document.getElementById('day').value);
-  let genderValue = document.querySelector('input[name="gender"]:checked').val();
+  let genderValue = document.querySelector('input[name="gender"]:checked').value;
 
-/*
+
   function dayLimit() {
     let maxday = document.getElementById("day");
     if (monthValue === "February" && parseInt(yearValue) % 4 === 0) {
@@ -22,7 +23,7 @@ function getAkanName () {
     }
   }
   dayLimit();
-*/
+
   let getCentury = function () {
     let centuryValue = parseInt(yearValue.slice(0,2));
     if (centuryValue < 17) {
@@ -55,32 +56,76 @@ function getAkanName () {
   let YY = parseInt(yearValue.slice(2,4));
   let DD = dayValue;
   let getKeyMonth = function () {
-    if (monthValue === "January") {
+    if (monthValue == "January" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 100 !== 0) {
+      return 1 - 1 - 1;
+    }else if(monthValue == "January" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 400 === 0) {
+      return 1 - 1 - 1;
+    }else if(monthValue == "January" && parseInt(yearValue) % 4 !== 0) {
       return 1 - 1;
-    }else if (monthValue === "February" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 100 !== 0) {
+    }else if (monthValue == "February" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 100 !== 0) {
       return 4 - 1;
-    }else if (monthValue === "February" && parseInt(yearValue) % 4 !== 0 ) {
+    }else if (monthValue == "February" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 400 === 0) {
+      return 4 - 1;
+    }else if (monthValue == "February" && parseInt(yearValue) % 4 !== 0 ) {
       return 4;
-    }else if (monthValue === "February" && parseInt(yearValue) % 100 === 0 ) {
+    }else if (monthValue == "March" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 100 !== 0) {
+      return 4 - 1;
+    }else if (monthValue == "March" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 400 === 0) {
+      return 4 - 1;
+    }else if (monthValue == "March" && parseInt(yearValue) % 4 !== 0) {
       return 4;
-    }else if (monthValue === "March") {
-      return 4;
-    }else if (monthValue === "April") {
-      return 0;
-    }else if (monthValue === "May") {
+    }else if (monthValue == "April" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 100 !== 0) {
+      return 0 - 1;
+    }else if (monthValue == "April" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 400 === 0) {
+        return 0 - 1;
+    }else if (monthValue == "April" && parseInt(yearValue) % 4 !== 0) {
+        return 0;
+    }else if (monthValue == "May" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 100 !== 0) {
+      return 2 - 1;
+    }else if (monthValue == "May" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 400 === 0) {
+      return 2 - 1;
+    }else if (monthValue == "May" && parseInt(yearValue) % 4 !== 0) {
       return 2;
-    }else if (monthValue === "June") {
+    }else if (monthValue == "June" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 100 !== 0) {
+      return 5 - 1;
+    }else if (monthValue == "June" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 400 === 0) {
+      return 5 - 1;
+    }else if (monthValue == "June" && parseInt(yearValue) % 4 !== 0) {
       return 5;
-    }else if (monthValue === "July") {
+    }else if (monthValue == "July" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 100 !== 0) {
+      return 0 - 1;
+    }else if (monthValue == "July" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 400 === 0) {
+      return 0 - 1;
+    }else if (monthValue == "July" && parseInt(yearValue) % 4 !== 0) {
       return 0;
-    }else if (monthValue === "August") {
+    }else if (monthValue == "August" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 100 !== 0) {
+      return 3 - 1;
+    }else if (monthValue == "August" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 400 === 0) {
+      return 3 - 1;
+    }else if (monthValue == "August" && parseInt(yearValue) % 4 !== 0) {
       return 3;
-    }else if (monthValue === "September") {
+    }else if (monthValue == "September" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 100 !== 0) {
+      return 6 - 1;
+    }else if (monthValue == "September" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 400 === 0) {
+      return 6 - 1;
+    }else if (monthValue == "September" && parseInt(yearValue) % 4 !== 0) {
       return 6;
-    }else if (monthValue === "October") {
+    }else if (monthValue == "October" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 100 !== 0) {
+      return 1 - 1;
+    }else if (monthValue == "October" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 400 === 0) {
+      return 1 - 1;
+    }else if (monthValue == "October" && parseInt(yearValue) % 4 !== 0) {
       return 1;
-    }else if (monthValue === "November") {
+    }else if (monthValue == "November" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 100 !== 0) {
+      return 4 - 1;
+    }else if (monthValue == "November" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 400 === 0) {
+      return 4 - 1;
+    }else if (monthValue == "November" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 100 !== 0) {
       return 4;
+    }else if (monthValue == "December" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 100 !== 0) {
+      return 6 - 1;
+    }else if (monthValue == "December" && parseInt(yearValue) % 4 === 0 && parseInt(yearValue) % 400 === 0) {
+      return 6 - 1;
     }else {
       return 6;
     }
@@ -88,6 +133,7 @@ function getAkanName () {
   MM = getKeyMonth();
 
   let dayOfTheWeek = (Math.floor(YY/4) + DD + MM + CC + YY) % 7;
+
 
   if (genderValue == "male") {
     switch (dayOfTheWeek) {
@@ -137,36 +183,44 @@ function getAkanName () {
   }else if (genderValue == "female") {
     switch (dayOfTheWeek) {
       case 0:
-        document.getElementById('result').textContent = "You were born on a ${daysofTheWeek[0]}: your Akan name is " + maleAkanNames[0];
+        document.getElementById('result').textContent = "You were born on a " + daysofTheWeek[0] + " : your Akan name is " + femaleAkanNames[0];
         document.getElementById('displayname').textContent = "Your Akan name";
+        return false;
         break;
       case 1:
-        document.getElementById('result').textContent = "You were born on a ${daysofTheWeek[1]}: your Akan name is " + maleAkanNames[1];
+        document.getElementById('result').textContent = "You were born on a " + daysofTheWeek[1] + " : your Akan name is " + femaleAkanNames[1];
         document.getElementById('displayname').textContent = "Your Akan name";
+        return false;
         break;
       case 2:
-        document.getElementById('result').textContent = "You were born on a ${daysofTheWeek[2]}: your Akan name is " + maleAkanNames[2];
+        document.getElementById('result').textContent = "You were born on a " + daysofTheWeek[2] + " : your Akan name is " + femaleAkanNames[2];
         document.getElementById('displayname').textContent = "Your Akan name";
+        return false;
         break;
       case 3:
-        document.getElementById('result').textContent = "You were born on a ${daysofTheWeek[3]}: your Akan name is " + maleAkanNames[3];
+        document.getElementById('result').textContent = "You were born on a " + daysofTheWeek[3] + " : your Akan name is " + femaleAkanNames[3];
         document.getElementById('displayname').textContent = "Your Akan name";
+        return false;
         break;
       case 4:
-        document.getElementById('result').textContent = "You were born on a ${daysofTheWeek[4]}: your Akan name is " + maleAkanNames[4];
+        document.getElementById('result').textContent = "You were born on a " + daysofTheWeek[4] + " : your Akan name is " + femaleAkanNames[4];
         document.getElementById('displayname').textContent = "Your Akan name";
+        return false;
         break;
       case 5:
-        document.getElementById('result').textContent = "You were born on a ${daysofTheWeek[5]}: your Akan name is " + maleAkanNames[5];
+        document.getElementById('result').textContent = "You were born on a " + daysofTheWeek[5] + " : your Akan name is " + femaleAkanNames[5];
         document.getElementById('displayname').textContent = "Your Akan name";
+        return false;
         break;
       case 6:
-        document.getElementById('result').textContent = "You were born on a ${daysofTheWeek[6]}: your Akan name is " + maleAkanNames[6];
+        document.getElementById('result').textContent = "You were born on a " + daysofTheWeek[6] + " : your Akan name is " + femaleAkanNames[6];
         document.getElementById('displayname').textContent = "Your Akan name";
+        return false;
         break;
       case 7:
-        document.getElementById('result').textContent = "You were born on a ${daysofTheWeek[7]}: your Akan name is " + maleAkanNames[7];
+        document.getElementById('result').textContent = "You were born on a " + daysofTheWeek[7] + " : your Akan name is " + femaleAkanNames[7];
         document.getElementById('displayname').textContent = "Your Akan name";
+        return false;
         break;
       default:
         alert("No Akan Name. Verify your inputs are correct.");
